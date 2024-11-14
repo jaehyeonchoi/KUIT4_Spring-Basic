5f7c09aa8f1277624e108227f81fc5e8e1cc3364
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-14 11:44:03 +0900

    feat(UserController):회원가입 form 출력 기능 구현

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,10 +1,22 @@
 package kuit.springbasic.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
     /**
      * TODO: showUserForm
      */
+    @RequestMapping("/form")
+    public String showUserForm() {
+        log.info("showUserForm");
+        return "/user/form";
+    }
 
     /**
      * TODO: createUser
