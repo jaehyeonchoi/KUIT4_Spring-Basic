c4adeb5cb6c0127576424a3d8419e03491b9bb7e
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-15 03:04:07 +0900

    feat(QnaController):showQnA 구현

--- src/main/java/kuit/springbasic/controller/qna/QnaController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnaController.java
@@ -1,9 +1,63 @@
 package kuit.springbasic.controller.qna;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.AnswerRepository;
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import kuit.springbasic.util.UserSessionUtils;
+
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/qna")
 public class QnaController {
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
 
     /**
      * TODO: showQnA
      */
+    @RequestMapping("/show")
+    public String showQnA(@RequestParam("questionId") String questionId,
+                          Model model) {
+        log.info("showQnA");
+        Question question = questionRepository.findByQuestionId(Integer.parseInt(questionId));
+        Collection<Answer> answers = answerRepository.findAllByQuestionId(question.getQuestionId());
 
+        model.addAttribute("question", question)
+                .addAttribute("answers", answers);
+        return "/qna/show";
+    }
+
+    /**
+     * TODO: showQuestionForm
+     */
+
+
+    /**
+     * TODO: createQuestion
+     * createQuestionV1 : @RequestParam
+     * createQuestionV2 : @ModelAttribute
+     */
+
+
+    /**
+     * TODO: showUpdateQuestionForm
+     * showUpdateQuestionFormV1 : @RequestParam, HttpServletRequest, Model
+     * showUpdateQuestionFormV2 : @RequestParam, @SessionAttribute, Model
+     */
+
+    /**
+     * TODO: updateQuestion
+     */
 }
