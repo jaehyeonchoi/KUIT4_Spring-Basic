cb66cbd3ec66407482c98853b52f9d7fe380d2b7
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-15 01:47:36 +0900

    feat(UserController):createUserV2 @ModelAttribute 방식 구현

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -5,6 +5,7 @@ import kuit.springbasic.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,13 +30,22 @@ public class UserController {
      * createUserV1 : @RequestParam
      * createUserV2 : @ModelAttribute
      */
-    @RequestMapping("signup")
+    //@RequestMapping("signup")
     public String createUserV1(@RequestParam("userId") String userId,
                              @RequestParam("password") String password,
                              @RequestParam("name") String name,
                              @RequestParam("email") String email) {
         log.info("createUser");
         User user = new User(userId, password, name, email);
+        userRepository.insert(user);
+        log.info("user 회원가입 완료");
+        return "redirect:/user/list";
+    }
+
+    @RequestMapping("signup")
+    public String createUserV2(@ModelAttribute User user) {
+        // @RequestParam을 쓸 때, 키 이름과 담을 변수명이 같으면 키 생략 가능
+        log.info("createUser");
         userRepository.insert(user);
         log.info("user 회원가입 완료");
         return "redirect:/user/list";
