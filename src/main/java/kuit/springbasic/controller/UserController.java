0844cb552c0a4fd43a43193d1a796ac703db0ed1
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-14 11:54:54 +0900

    feat(UserController):createUserV1 @RequestParam 방식 구현

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,13 +1,19 @@
 package kuit.springbasic.controller;
 
+import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserRepository userRepository;
 
     /**
      * TODO: showUserForm
@@ -23,6 +29,17 @@ public class UserController {
      * createUserV1 : @RequestParam
      * createUserV2 : @ModelAttribute
      */
+    @RequestMapping("signup")
+    public String createUserV1(@RequestParam("userId") String userId,
+                             @RequestParam("password") String password,
+                             @RequestParam("name") String name,
+                             @RequestParam("email") String email) {
+        log.info("createUser");
+        User user = new User(userId, password, name, email);
+        userRepository.insert(user);
+        log.info("user 회원가입 완료");
+        return "redirect:/user/list";
+    }
 
     /**
      * TODO: showUserList
