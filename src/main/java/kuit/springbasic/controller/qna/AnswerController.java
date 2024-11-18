8ad9057675f1e0df2639c1221809e3c70a0bf649
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-18 20:52:51 +0900

    refactor(AnswerController):불필요한 import 제거

--- src/main/java/kuit/springbasic/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/controller/qna/AnswerController.java
@@ -8,7 +8,6 @@ import kuit.springbasic.domain.Question;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import kuit.springbasic.domain.Answer;
